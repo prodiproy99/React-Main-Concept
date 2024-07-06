e6d7e3d7049eb9d6e07adab2e7acfc67545a6751
Prodip Roy <rprodipchandro@gmail.com>
2024-07-06 19:36:05 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+ 
+import './App.css'
+import Countries from './Components/Countries/Countries'
+
+function App() {
+  
+
+  return (
+    <>
+    <Countries></Countries>
+      <h1>React</h1>
+    </>
+  )
+}
+
+export default App
---
+++ src/Components/Countries/Countries.jsx
@@ -0,0 +1,26 @@
+import { useEffect } from "react";
+import { useState } from "react";
+import Country from "./Country";
+import './Countries.css'
+
+const Countries = () => {
+    const [countries, setCountries] = useState([])
+    useEffect( () =>{
+        fetch('https://restcountries.com/v3.1/all')
+        .then(res => res.json())
+        .then(data => setCountries(data))
+    }, [])
+
+    return (
+        <div className="Countries">
+            {/* <h2>Countries: {countries.length}</h2> */}
+            {
+                countries.map(country => <Country
+                    key={country.cca3}
+                    country={country}></Country>)
+            }
+        </div>
+    );
+};
+
+export default Countries;
